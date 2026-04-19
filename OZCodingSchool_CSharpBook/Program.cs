082191773d9f475e4890a9da0d15217e483c57ec
LeeKiWoong <littlemanner1@gmail.com>
2026-04-19 15:45:51 +0900

    [Page - 048] using static 구문으로 System.Console 생략해보기!
    이런 것도 가능하다니... ㄷㄷ

--- OZCodingSchool_CSharpBook/Program.cs
+++ OZCodingSchool_CSharpBook/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace OZCodingSchool_CSharpBook
 {
-    class UsingDemo
+    class WriteLineDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteLine("명령 프롬프트에 출력할 내용");
         }
     }
 }
