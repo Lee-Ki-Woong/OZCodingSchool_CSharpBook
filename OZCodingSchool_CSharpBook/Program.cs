699237617f0afb96f142b1e20d1ac1cde059eea8
LeeKiWoong <littlemanner1@gmail.com>
2026-04-22 04:34:40 +0900

    [Page - 068] 변수를 만들고 값 저장 후 사용
    변수 i를 만들고 정수형 리터럴 1234를 대입하여 출력하였다.

--- OZCodingSchool_CSharpBook/Program.cs
+++ OZCodingSchool_CSharpBook/Program.cs
@@ -2,14 +2,13 @@
 
 namespace OZCodingSchool_CSharpBook
 {
-    class Literal
+    class VriableNote
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( 1234);
-            Console.WriteLine( 3.14F);
-            Console.WriteLine( 'A');
-            Console.WriteLine( "HELLO");
+            int i;
+            i = 1234;
+            Console.WriteLine( i);
         }
     }
 }
