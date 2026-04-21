781d5d2ea6fee9f01acf4a3a614180e46e67ad75
LeeKiWoong <littlemanner1@gmail.com>
2026-04-22 04:47:05 +0900

    [Page - 070] 변수 여러 개를 한 번에 선언
    이건 모르던 선언 방식이네요! 근데 실무에서 쓸 일이 있을까...?

--- OZCodingSchool_CSharpBook/Program.cs
+++ OZCodingSchool_CSharpBook/Program.cs
@@ -2,12 +2,17 @@
 
 namespace OZCodingSchool_CSharpBook
 {
-    class VriableDemo
+    class VriableComma
     {
         static void Main(string[] args)
         {
-            int number = 7;
-            Console.WriteLine( "{0}", number);
+            int number1, number2, number3;
+
+            number1 = 1;
+            number2 = 2;
+            number3 = 3;
+
+            Console.WriteLine("{0},{1},{2}", number1, number2, number3);
         }
     }
 }
