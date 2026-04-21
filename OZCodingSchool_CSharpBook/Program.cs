b1d045a657c006b8d67f755cf99e02b7c6c5fbac
LeeKiWoong <littlemanner1@gmail.com>
2026-04-22 03:02:44 +0900

    [Page - 065 / Self]한글과 한자로 변수 만들어보기
    이게 되네 ㄷㄷ

--- OZCodingSchool_CSharpBook/Program.cs
+++ OZCodingSchool_CSharpBook/Program.cs
@@ -6,6 +6,10 @@ namespace OZCodingSchool_CSharpBook
     {
         static void Main(string[] args)
         {
+            int 이게된다고 = 1;
+            int 時市施 = 2;
+            Console.WriteLine(이게된다고);
+            Console.WriteLine(時市施);
         }
     }
 }
